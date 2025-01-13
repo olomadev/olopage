a9786c946b28e3bc09d5539bfe14fdc45093899c
eguvenc <eguvenc@gmail.com>
2025-01-13 13:50:08 +0300

    added backend

---
+++ core/server.js
@@ -0,0 +1,34 @@
+/**
+ * Olopage core server.js - v1.0 - 2025
+ * 
+ * Core server.js - do not touch this file it can be
+ * change when update your application
+ */
+import express from "express";
+import expressLayouts from 'express-ejs-layouts';
+import ViteExpress from "vite-express";
+import config from '../../config.json' assert { type: 'json' };
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// const themeConfigPath = `./themes/${config.theme}/config.json`;
+// const themeConfig = JSON.parse(fs.readFileSync(themeConfigPath, "utf8"));
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // set the global views directory
+app.use(expressLayouts);
+app.use("/themes", express.static(path.join(__dirname, `/themes/${config.theme}/assets`)));
+app.use(express.static(path.join(__dirname, "../public")));
+
+const themePath = `../themes/${config.theme}/server.js`;
+import(themePath)
+  .then(({ default: themeServer }) => themeServer(app, config))
+  .catch((err) => console.error("Theme could not be loaded:", err));
+
+ViteExpress.listen(app, config.port, () =>
+  console.log("Server is listening on port " + config.port + "..."),
+);
--- themes/clean-blog/server.js
+++ themes/clean-blog/server.js
@@ -1,77 +1,61 @@
-import express from "express";
-import expressLayouts from 'express-ejs-layouts';
-import ViteExpress from "vite-express";
-
-import config from '../../config.json' assert { type: 'json' };
-
-import { sequelize } from './db.js'; // Import sequelize instance
-import { Posts } from './models/post.js';  // Import Post model
-import { Users } from './models/users.js';  // Import User model
+/**
+ * Clean-blog default theme v1.0 - 2025
+ */
+import expressLayouts from "express-ejs-layouts";
+import { Posts } from "./models/posts.js";
+import { Users } from "./models/users.js";
 import { cacheQueryResults } from './cache.js'; // Import the cache helper
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-const themeConfigPath = `./themes/${config.theme}/config.json`;
-const themeConfig = JSON.parse(fs.readFileSync(themeConfigPath, "utf8"));
-
-// Define the reverse relationship
 Posts.belongsTo(Users, { foreignKey: 'authorId' });
 Users.hasMany(Posts, { foreignKey: 'authorId' });
 
-const app = express();
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); // set the global views directory
-app.use(expressLayouts);
-app.use("/themes", express.static(path.join(__dirname, `/themes/${config.theme}/assets`)));
-app.use(express.static(path.join(__dirname, 'public')));
-//
-// set default layout
-//
-app.set('layout', 'layouts/default');
+export default function themeServer(app, config) {
+  //
+  // set default layout
+  //
+  app.set('layout', 'layouts/default');
 
-app.get("/", async (req, res) => {
+  // app.use(expressLayouts);
+  // app.use("/themes", express.static("assets"));
 
-  const posts = await cacheQueryResults("olopage:posts-all", async () => {
-    return await Posts.findAll({
-      include: [{
-        model: Users,
-        required: true,  // This will perform an INNER JOIN to fetch associated users
-        attributes: ['userId', 'email', 'firstname', 'lastname'],  // Fetch specific user fields
-      }],
+  app.get("/", async (req, res) => {
+    const posts = await cacheQueryResults("olopage:posts-all", async () => {
+      return await Posts.findAll({
+        include: [{
+          model: Users,
+          required: true,  // This will perform an INNER JOIN to fetch associated users
+          attributes: ['userId', 'email', 'firstname', 'lastname'],  // Fetch specific user fields
+        }],
+      });
+    });
+    // Render the data to the view
+    res.render('index', {
+      title: 'Welcome !',
+      keywords: '',
+      description: '',
+      themePath: config.path,
+      posts: posts,  // Pass the fetched or cached posts to the view
     });
   });
 
-  // Render the data to the view
-  res.render('index', {
-    title: 'Welcome !',
-    keywords: '',
-    description: '',
-    themePath: `themes/${config.theme}`,
-    posts: posts,  // Pass the fetched or cached posts to the view
+  app.get("/about", async (req, res) => {
+    res.render('about', { themePath: config.path });
   });
-});
 
-app.get("/about", async (req, res) => {
-  res.render('about', { themePath: `themes/${config.theme}`});
-});
-app.get("/contact", async (req, res) => {
-  res.render('contact', { themePath: `themes/${config.theme}`});
-});
-app.get("/:slug", async (req, res) => {
-  const slug = req.params.slug;
-  const post = await Posts.findOne({ where: { slug: slug } });
-  if (!post) {
-    return res.status(404).render("404");
-  }
-  res.render('post', {
-    themePath: `themes/${config.theme}`,
-    post,
+  app.get("/contact", async (req, res) => {
+    res.render('contact', { themePath: config.path });
   });
-});
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
-);
+  app.get("/:slug", async (req, res) => {
+    const slug = req.params.slug;
+    const post = await Posts.findOne({ where: { slug: slug } });
+    if (!post) {
+      return res.status(404).render("404");
+    }
+    res.render('post', {
+      themePath: config.path,
+      post,
+    });
+  });
+
+}
